12e243ab596070cc736c421f3bc3aa191fde9ee9
Matheus Mauricio <lhr3@hotmail.com>
2024-06-29 21:44:43 -0300

    add: devolucao tests to get 100% of test cov

--- test/test_service/test_devolucao_service.py
+++ test/test_service/test_devolucao_service.py
@@ -1,3 +1,4 @@
+import pytest
 from src.models.models import Devolucao, Emprestimo, SituacaoExemplar
 
 def test_may_create_devolucao_and_return_201(client, db_for_emprestimo):
@@ -51,3 +52,190 @@ def test_may_create_devolucao_and_return_201(client, db_for_emprestimo):
     assert data['id'] == devolucao_db.id
     assert data['emprestimo_id'] == devolucao_db.emprestimo_id
     assert data['funcionario_id'] == devolucao_db.funcionario_id
+
+def test_may_not_create_devolucao_and_return_422(client, create_and_drop_tables):
+    
+    devolucao_data = {
+        'emprestimo_id': '1',
+    }
+
+    response = client.post('/api/v1/devolucao', json=devolucao_data)
+    devolucao = response.json
+    assert response.status_code == 422
+    assert 'error' in devolucao.keys()
+    assert devolucao['error'] == 'Parametros invalidos no payload'
+
+
+devolucao_data = {
+    'emprestimo_id': '1',
+    'funcionario_id': '1'
+}
+
+@pytest.mark.parametrize(
+    'devolucao_data, erro, repository, method',
+    [
+        (
+            devolucao_data,
+            'Internal Server Error',
+            'src.repository.emprestimo.repository.EmprestimoRepository', 
+            'get_emprestimo_by_id'
+        ),
+        (
+            devolucao_data,
+            'Internal Server Error',
+            'src.repository.funcionario.repository.FuncionarioRepository', 
+            'get_funcionario_by_id'
+        ),
+        (
+            devolucao_data,
+            'Internal Server Error',
+            'src.models.models.Devolucao', 
+            '__init__'
+        ),
+        (
+            devolucao_data,
+            'Internal Server Error',
+            'src.repository.devolucao.repository.DevolucaoRepository', 
+            'make_devolucao'
+        ),
+    ]
+)
+def test_may_not_create_devolucao_and_return_500(
+        client, mocker, db_for_emprestimo,
+        repository, erro, method, devolucao_data
+):
+    emprestimo_data = {
+        'exemplar_id': '123',
+        'funcionario_id': '1',
+        'usuario_id': '1'
+    }
+
+    # Criar um empréstimo
+    response_1 = client.post('/api/v1/emprestimo', json=emprestimo_data)
+    assert response_1.status_code == 201
+
+    mocker.patch(
+        f'{repository}.{method}', 
+        side_effect=Exception('Database error')
+    )
+
+    response = client.post('/api/v1/devolucao', json=devolucao_data)
+    devolucao = response.json
+    assert response.status_code == 500
+    assert 'error' in devolucao.keys()
+    assert devolucao['error'] == erro
+
+
+def test_may_not_create_devolucao_by_None_return_and_return_500(
+        client, mocker, db_for_emprestimo
+):
+    emprestimo_data = {
+        'exemplar_id': '123',
+        'funcionario_id': '1',
+        'usuario_id': '1'
+    }
+
+    devolucao_data = {
+        'emprestimo_id': '1',
+        'funcionario_id': '1'
+    }
+
+    # Criar um empréstimo
+    response_1 = client.post('/api/v1/emprestimo', json=emprestimo_data)
+    assert response_1.status_code == 201
+
+    mocker.patch(
+        f'src.repository.devolucao.repository.DevolucaoRepository.make_devolucao', 
+        return_value=None
+    )
+
+    response = client.post('/api/v1/devolucao', json=devolucao_data)
+    devolucao = response.json
+    assert response.status_code == 500
+    assert 'error' in devolucao.keys()
+    assert devolucao['error'] == 'Internal Server Error'
+
+    devolucao = Devolucao.query.all()
+    assert len(devolucao) == 0
+
+devolucao_data = {
+    'emprestimo_id': '2',
+    'funcionario_id': '1'
+}
+@pytest.mark.parametrize(
+        'devolucao_data, erro',
+        [
+            (
+                {
+                    'emprestimo_id': '2',
+                    'funcionario_id': '1'
+                }, 
+                'Emprestimo not found'
+            ),
+            (
+                {
+                    'emprestimo_id':'1',
+                    'funcionario_id':'2'
+                },
+                'Funcionario not found'
+            )
+        ]
+)
+def test_may_not_create_devolucao_and_return_404(
+    client, db_for_emprestimo,
+    devolucao_data, erro
+):
+        
+    emprestimo_data = {
+        'exemplar_id': '123',
+        'funcionario_id': '1',
+        'usuario_id': '1'
+    }
+
+    # Criar um empréstimo
+    response_1 = client.post('/api/v1/emprestimo', json=emprestimo_data)
+    assert response_1.status_code == 201
+
+    response = client.post('/api/v1/devolucao', json=devolucao_data)
+    devolucao = response.json
+    assert response.status_code == 404
+    assert 'error' in devolucao.keys()
+    assert devolucao['error'] == erro
+
+    devolucao = Devolucao.query.all()
+    assert len(devolucao) == 0
+
+def test_may_not_duplicate_devolucao_and_return_409(
+    client, db_for_emprestimo,
+):
+    emprestimo_data = {
+        'exemplar_id': '123',
+        'funcionario_id': '1',
+        'usuario_id': '1'
+    }
+
+    devolucao_data = {
+        'emprestimo_id': '1',
+        'funcionario_id': '1'
+    }
+    
+    # Criar um empréstimo
+    response_1 = client.post('/api/v1/emprestimo', json=emprestimo_data)
+    assert response_1.status_code == 201
+
+    response = client.post('/api/v1/devolucao', json=devolucao_data)
+    devolucao = response.json
+    assert response.status_code == 201
+
+    devolucao = Devolucao.query.all()
+    assert len(devolucao) == 1
+
+    response = client.post('/api/v1/devolucao', json=devolucao_data)
+    devolucao = response.json
+    assert response.status_code == 409
+
+    assert 'error' in devolucao.keys()
+    assert devolucao['error'] == 'Emprestimo ja finalizado'
+
+    devolucao = Devolucao.query.all()
+    assert len(devolucao) == 1
